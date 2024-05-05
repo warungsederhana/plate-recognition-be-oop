c33312aa7202edbe93fe8d055e0222f2a68b3ec9
gybran <charistomarcgybran@students.undip.ac.id>
2024-05-05 21:32:27 +0700

    mengembalikan perubahan dari check ci cd

--- routes/main.route.js
+++ routes/main.route.js
@@ -5,7 +5,7 @@ const mainController = new MainController();
 const middleware = require("../middlewares/auth.middleware");
 
 // memberikan middleware otentikasi ke semua route main
-// router.use([middleware.isUserMiddleware, middleware.isAdminMiddleware]);
+router.use([middleware.isUserMiddleware, middleware.isAdminMiddleware]);
 router.get("/", (req, res) => mainController.getAllData(req, res));
 
 module.exports = router;
